e631e1eb60cd839bc635c1e2a7580a3e3fdc91ac
maryummm <80543776+maryummm@users.noreply.github.com>
2021-05-17 11:23:22 +0530

    Add files via upload

---
+++ ColorPalette.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, StyleSheet, FlatList,View } from 'react-native';
+import { roundToNearestPixel } from 'react-native/Libraries/Utilities/PixelRatio';
+import ColorBox from '../components/ColorBox';
+
+
+const ColorPalette = ({route}) => {
+    const {colors}= route.params;
+  return (
+    <FlatList
+      style={styles.container}
+      data={colors}
+      keyExtractor={item => item.hexCode}
+      renderItem={({ item }) => (
+        <ColorBox hexCode={item.hexCode} colorName={item.colorName} />
+      )}
+     // ListHeaderComponent={<Text style={styles.heading}>{paletteName}</Text>}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});
+
+export default ColorPalette;
